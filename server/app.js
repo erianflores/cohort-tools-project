e8081d64dc8fad55fb11c599950dd945b0135fd4
Erian Flores <erian.flores@gmail.com>
2025-01-09 18:41:00 +0100

    Updated

--- server/app.js
+++ server/app.js
@@ -7,10 +7,8 @@ const PORT = 5005;
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 // ...
 
-
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -21,7 +19,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
@@ -29,6 +26,9 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
 
 // START SERVER
 app.listen(PORT, () => {
