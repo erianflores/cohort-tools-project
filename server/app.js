b003ed275acd62febd9ca468f579be1cec07bda2
teresagarcia1985 <tre.trs@gmail.com>
2025-01-11 15:29:44 +0000

    wahtever

--- server/app.js
+++ server/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
-const cohorts = require("./cohorts.json");
-require("./db/index");
-const students = require("./students.json");
+const cohorts = require("./cohorts.json")
+require ("./db/index")
+const students = require("./students.json")
+const CohortModel = require("./models/User.model")
+const StudentModel = require("./models/User.model")
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -21,6 +24,7 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
@@ -40,3 +44,4 @@ app.get("/api/students", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
