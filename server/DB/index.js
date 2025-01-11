129bb779591bfa91d8c72a48096c9b3779229b81
Anna Turek <anutkakpi@ukr.net>
2025-01-11 17:33:31 +0200

    Done

--- server/DB/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect("mongodb://localhost:27017/Mini_Project")
-  .then(() => {
-    console.log("Conected to the database");
-  })
-  .catch((err) => console.log(err));
